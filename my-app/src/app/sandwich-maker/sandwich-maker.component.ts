64b2d9ba59f11a9aa806a6a11deb8d2d59318f08
ColeKedz <colerkedz@gmail.com>
2024-01-11 13:54:37 -0500

    order sandwhich form working without validations

--- my-app/src/app/sandwich-maker/sandwich-maker.component.ts
+++ my-app/src/app/sandwich-maker/sandwich-maker.component.ts
@@ -1,18 +1,67 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
+import { ReactiveFormsModule } from "@angular/forms";
 
 @Component({
   selector: 'sandwich-maker',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './sandwich-maker.component.html',
   styleUrl: './sandwich-maker.component.css'
 })
+
 export class SandwichMakerComponent {
 
-  breads: string[] = ['White', 'Wheat', 'Rye'];
-  meats: string[] = ['Chicken', 'Beef', 'none'];
-  vegetables: string[] = ['Tomatoe', 'Lettuce', 'Banana Peppers', 'Onion'];
-  cheeses: string[] = ['Cheddar', 'Pepper Jack', 'American', 'Provolone'];
+  data2 = {
+    breads: ['White', 'Wheat', 'Rye'],
+    meats: ['Chicken', 'Roast Beef', "Turkey", "Pastrami"],
+    vegetables: ['Tomatoe', 'Lettuce', 'Banana Peppers', 'Onion'],
+    cheeses: ['Cheddar', 'Pepper Jack', 'American', 'Provolone'],
+    condiments: ['Mayonnaise', 'Olive Oil', 'Vinegar', 'Pepper', 'Salt', 'Oregano']
+  }
+
+  orderForm: FormGroup;
+
+  // create form with formbuilder and designate form arrays and controlls.
+  // FormArrays will handle multiple selections with check boxes
+  constructor(private form: FormBuilder) {
+    this.orderForm = this.form.group({
+      name: new FormControl(null, Validators.required),
+      breads: new FormControl(null, Validators.required),
+      meats: new FormArray([]),
+      vegetables: new FormArray([]),
+      cheeses: new FormArray([]),
+      condiments: new FormArray([])
+    })
+  }
+
+  protected handleSelections(e: any) {
+
+    let selectedArray = this.orderForm.get(e.target.name) as FormArray;
+
+    if (e.target.checked) {
+
+      selectedArray.push(new FormControl(e.target.value));
+    }
+
+    else {
+      let index = 0;
+      selectedArray.controls.forEach((option: any) => {
+        if (option.value == e.target.value) {
+          selectedArray.removeAt(index)
+          return
+        }
+        index++
+      })
+
+    }
+
+  }
+
+
+  protected createOrder() {
+    console.log(this.orderForm.value);
+  }
 
 }
